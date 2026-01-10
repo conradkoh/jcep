6efff405ff0ebf0f32721df980737ec4e195279c
conradkoh <conradkoh@gmail.com>
2026-01-11 03:04:31 +0800

    hide app version

--- apps/webapp/src/app/page.tsx
+++ apps/webapp/src/app/page.tsx
@@ -2,11 +2,7 @@
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
-import { useAppVersion } from '@/modules/app/useAppInfo';
-
 export default function Home() {
-  const appVersion = useAppVersion();
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[24px] row-start-2 max-w-2xl">
@@ -27,9 +23,7 @@ export default function Home() {
           </Link>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <div>App Version: {appVersion ?? 'Loading...'}</div>
-      </footer>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center" />
     </div>
   );
 }
