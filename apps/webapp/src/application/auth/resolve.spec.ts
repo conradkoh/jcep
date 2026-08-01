conradkoh/next-convex-starter-app

feat(rbac): add users.roleNames for multi-role assignment (Phase 1b) (#94)
* feat(rbac): add users.roleNames for multi-role assignment (Phase 1b)

Introduce roleNames on the users schema with accessLevel fallback, enable the manager role, and add a migration to backfill existing users.

Co-authored-by: Cursor <cursoragent@cursor.com>

* fix(rbac): always grant system_admin from accessLevel without migration

Check accessLevel before roleNames so system administrators retain full access even when roleNames is unset or conflicting.

Co-authored-by: Cursor <cursoragent@cursor.com>

* chore(rbac): colocate auth unit tests and document PR #94 reference

Move application/auth specs beside source files per testing guide.
Add Phase 1b reference implementation section linking PR #94 for downstream repos.

Co-authored-by: Cursor <cursoragent@cursor.com>

---------

Co-authored-by: Cursor <cursoragent@cursor.com>

apps/webapp/src/application/auth/resolve.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { allPermissions, type Permission } from '../permissions';
+import { allPermissions, type Permission } from './permissions';
 import {
   getPermissionsForUser,
   getResolvedPermissionsForUser,
@@ -8,8 +8,8 @@ import {
   hasPermission,
   permissionGrantMatches,
   unionPermissionsForRoles,
-} from '../resolve';
-import { systemAdminPermissions } from '../roles';
+} from './resolve';
+import { systemAdminPermissions } from './roles';
 
 describe('permissionGrantMatches', () => {
   it('matches exact permissions', () => {
@@ -33,6 +33,56 @@ describe('getRolesForUser', () => {
     expect(getRolesForUser({ accessLevel: undefined })).toEqual(['user']);
     expect(getRolesForUser({ accessLevel: 'user' })).toEqual(['user']);
     expect(getRolesForUser({ accessLevel: 'system_admin' })).toEqual(['system_admin']);
+  });
+});
+
+describe('getRolesForUser with roleNames', () => {
+  it('reads roleNames when present', () => {
+    expect(getRolesForUser({ roleNames: ['user', 'manager'] })).toEqual(['user', 'manager']);
+  });
+
+  it('falls back to accessLevel when roleNames is empty', () => {
+    expect(getRolesForUser({ roleNames: [], accessLevel: 'system_admin' })).toEqual([
+      'system_admin',
+    ]);
+  });
+
+  it('falls back to accessLevel when roleNames is undefined', () => {
+    expect(getRolesForUser({ accessLevel: 'user' })).toEqual(['user']);
+  });
+
+  it('filters unknown role names', () => {
+    expect(getRolesForUser({ roleNames: ['user', 'nonexistent'] })).toEqual(['user']);
+  });
+
+  it('falls back to accessLevel when all roleNames are unknown', () => {
+    expect(getRolesForUser({ roleNames: ['bogus'], accessLevel: 'system_admin' })).toEqual([
+      'system_admin',
+    ]);
+  });
+
+  it('always grants system_admin when accessLevel is system_admin, regardless of roleNames', () => {
+    expect(getRolesForUser({ accessLevel: 'system_admin', roleNames: ['user'] })).toEqual([
+      'system_admin',
+    ]);
+    expect(getRolesForUser({ accessLevel: 'system_admin' })).toEqual(['system_admin']);
+  });
+});
+
+describe('hasPermission with roleNames', () => {
+  it('unions permissions across multiple roles', () => {
+    const user = { roleNames: ['user', 'manager'] };
+    expect(hasPermission(user, 'attendance:read')).toBe(true);
+    expect(hasPermission(user, 'users:list')).toBe(true);
+    expect(hasPermission(user, 'system_admin:access')).toBe(false);
+  });
+
+  it('grants system admin permissions without roleNames migration', () => {
+    const user = { accessLevel: 'system_admin' as const };
+    expect(hasPermission(user, 'system_admin:access')).toBe(true);
+    expect(
+      hasPermission({ accessLevel: 'system_admin', roleNames: ['user'] }, 'system_admin:access')
+    ).toBe(true);
   });
 });
 

conradkoh/jcep

apps/webapp/src/application/auth/resolve.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { allPermissions, type Permission } from '../permissions';
+import { allPermissions, type Permission } from './permissions';
 import {
   getPermissionsForUser,
   getResolvedPermissionsForUser,
@@ -8,8 +8,8 @@ import {
   hasPermission,
   permissionGrantMatches,
   unionPermissionsForRoles,
-} from '../resolve';
-import { systemAdminPermissions } from '../roles';
+} from './resolve';
+import { systemAdminPermissions } from './roles';
 
 describe('permissionGrantMatches', () => {
   it('matches exact permissions', () => {
@@ -36,14 +36,62 @@ describe('getRolesForUser', () => {
   });
 
   it('resolves jcep_admin via roleNames', () => {
-    expect(getRolesForUser({ accessLevel: 'user', roleNames: ['jcep_admin'] })).toEqual([
-      'user',
-      'jcep_admin',
-    ]);
+    expect(getRolesForUser({ roleNames: ['jcep_admin'] })).toEqual(['jcep_admin']);
     expect(getRolesForUser({ accessLevel: 'system_admin', roleNames: ['jcep_admin'] })).toEqual([
       'system_admin',
       'jcep_admin',
     ]);
+  });
+});
+
+describe('getRolesForUser with roleNames', () => {
+  it('reads roleNames when present', () => {
+    expect(getRolesForUser({ roleNames: ['user', 'manager'] })).toEqual(['user', 'manager']);
+  });
+
+  it('falls back to accessLevel when roleNames is empty', () => {
+    expect(getRolesForUser({ roleNames: [], accessLevel: 'system_admin' })).toEqual([
+      'system_admin',
+    ]);
+  });
+
+  it('falls back to accessLevel when roleNames is undefined', () => {
+    expect(getRolesForUser({ accessLevel: 'user' })).toEqual(['user']);
+  });
+
+  it('filters unknown role names', () => {
+    expect(getRolesForUser({ roleNames: ['user', 'nonexistent'] })).toEqual(['user']);
+  });
+
+  it('falls back to accessLevel when all roleNames are unknown', () => {
+    expect(getRolesForUser({ roleNames: ['bogus'], accessLevel: 'system_admin' })).toEqual([
+      'system_admin',
+    ]);
+  });
+
+  it('always grants system_admin when accessLevel is system_admin, stacking known roleNames', () => {
+    expect(getRolesForUser({ accessLevel: 'system_admin', roleNames: ['user'] })).toEqual([
+      'system_admin',
+      'user',
+    ]);
+    expect(getRolesForUser({ accessLevel: 'system_admin' })).toEqual(['system_admin']);
+  });
+});
+
+describe('hasPermission with roleNames', () => {
+  it('unions permissions across multiple roles', () => {
+    const user = { roleNames: ['user', 'manager'] };
+    expect(hasPermission(user, 'attendance:read')).toBe(true);
+    expect(hasPermission(user, 'users:list')).toBe(true);
+    expect(hasPermission(user, 'system_admin:access')).toBe(false);
+  });
+
+  it('grants system admin permissions without roleNames migration', () => {
+    const user = { accessLevel: 'system_admin' as const };
+    expect(hasPermission(user, 'system_admin:access')).toBe(true);
+    expect(
+      hasPermission({ accessLevel: 'system_admin', roleNames: ['user'] }, 'system_admin:access')
+    ).toBe(true);
   });
 });
 